b080d66415f452253e19a2cca35c1825968407df
atopx <3940422@qq.com>
2024-08-28 18:56:39 +0800

    fix notification

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,53 +1,27 @@
 <!-- src/App.vue -->
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import Loading from './components/Loading.vue'; // 加载动画组件
-import Game from './Game.vue';
-import {EventsOn} from "../wailsjs/runtime";
-import {GetState} from "../wailsjs/go/api/Api";
-import {useNotification} from "naive-ui";
+import { onMounted, ref } from "vue";
+import Loading from "./components/Loading.vue"; // 加载动画组件
+import Game from "./Game.vue";
+import { EventsOn } from "../wailsjs/runtime";
+import { GetState } from "../wailsjs/go/api/Api";
 
-const GameState = ref("Disconnect")
-
-interface notification {
-  level: string;
-  message: string;
-}
-
-const notice = useNotification();
+const GameState = ref("Disconnect");
 
 onMounted(() => {
-  GetState().then((value: string) => {
-    GameState.value = value
-  })
-  EventsOn("GameState", function (state: string) {
-    GameState.value = state;
-  })
-  EventsOn("notification", async function (data: notification) {
-    switch (data.level) {
-      case "info":
-        notice.info({content: data.message})
-        break
-      case "warn":
-        notice.warning({content: data.message})
-        break
-      case "error":
-        notice.error({content: data.message})
-        break
-      case "success":
-        notice.error({content: data.message})
-        break
-    }
-  })
-
-
+    GetState().then((value: string) => {
+        GameState.value = value;
+    });
+    EventsOn("GameState", function (state: string) {
+        GameState.value = state;
+    });
 });
 </script>
 
 <template>
-  <n-notification-provider>
-    <!--  <Loading v-if="(GameState === 'Disconnect')"/>-->
-    <Loading v-if="(GameState === 'Disconnect')"/>
-    <Game v-else/>
-  </n-notification-provider>
+    <n-notification-provider>
+        <!--  <Loading v-if="(GameState === 'Disconnect')"/>-->
+        <Loading v-if="GameState === 'Disconnect'" />
+        <Game v-else />
+    </n-notification-provider>
 </template>
--- frontend/src/Game.vue
+++ frontend/src/Game.vue
@@ -1,82 +1,118 @@
 <template>
-  <n-layout has-sider style="height: 100vh">
-    <n-layout-sider
-        class="main-sider"
-        bordered
-        :collapsed-width="64"
-        :width="100"
-    >
-      <n-menu
-          :indent="16"
-          :collapsed-width="64"
-          :collapsed-icon-size="22"
-          :options="topMenuOptions"
-          @update:value="handleMenuChange"
-      />
-      <n-menu
-          class="bottom-menu"
-          :indent="16"
-          :collapsed-width="64"
-          :collapsed-icon-size="22"
-          :options="bottomMenuOptions"
-          @update:value="handleMenuChange"
-      />
-    </n-layout-sider>
-    <n-layout>
-      <Career v-if="currentMenu === 'career'"></Career>
-      <Record v-if="currentMenu === 'record'"></Record>
-      <Assistant v-if="currentMenu === 'assistant'"></Assistant>
-      <!--        <Personal v-if="currentMenu === 'personal'"></Personal>-->
-      <Setting v-if="currentMenu === 'setting'"></Setting>
+    <n-layout has-sider style="height: 100vh">
+        <n-layout-sider class="main-sider" bordered :collapsed-width="64" :width="100">
+            <n-menu
+                :indent="16"
+                :collapsed-width="64"
+                :collapsed-icon-size="22"
+                :options="topMenuOptions"
+                @update:value="handleMenuChange"
+            />
+            <n-menu
+                class="bottom-menu"
+                :indent="16"
+                :collapsed-width="64"
+                :collapsed-icon-size="22"
+                :options="bottomMenuOptions"
+                @update:value="handleMenuChange"
+            />
+        </n-layout-sider>
+        <n-layout>
+            <Career v-if="currentMenu === 'career'"></Career>
+            <Record v-if="currentMenu === 'record'"></Record>
+            <Assistant v-if="currentMenu === 'assistant'"></Assistant>
+            <!--        <Personal v-if="currentMenu === 'personal'"></Personal>-->
+            <Setting v-if="currentMenu === 'setting'"></Setting>
+        </n-layout>
     </n-layout>
-  </n-layout>
 </template>
 
-
 <script setup lang="ts">
-import {h, ref} from 'vue';
-import {NLayout, NLayoutSider, NMenu} from 'naive-ui';
+import { h, onMounted, ref } from "vue";
+import { NLayout, NLayoutSider, NMenu } from "naive-ui";
 import Career from "./views/Career.vue";
 import Record from "./views/Record.vue";
 import Assistant from "./views/Assistant.vue";
 import Setting from "./views/Setting.vue";
-import CareerIcon from '/src/assets/icons/Career.svg';
-import RecordIcon from '/src/assets/icons/Record.svg';
-import AssistantIcon from '/src/assets/icons/Game.svg';
+import CareerIcon from "/src/assets/icons/Career.svg";
+import RecordIcon from "/src/assets/icons/Record.svg";
+import AssistantIcon from "/src/assets/icons/Game.svg";
 // import PersonalIcon from '/src/assets/icons/User.svg';
-import SettingIcon from '/src/assets/icons/Setting.svg';
+import SettingIcon from "/src/assets/icons/Setting.svg";
+import { EventsOn } from "../wailsjs/runtime/runtime";
+import { useNotification } from "naive-ui";
 
 // 响应式状态
-const currentMenu = ref('assistant');
+const currentMenu = ref("assistant");
 
 // 菜单选项
 const topMenuOptions = [
-  {label: '生涯', key: 'career', icon: () => h('img', {src: CareerIcon, style: 'width: 20px; height: 20px;'})},
-  {label: '战绩', key: 'record', icon: () => h('img', {src: RecordIcon, style: 'width: 20px; height: 20px;'})},
-  {label: '助手', key: 'assistant', icon: () => h('img', {src: AssistantIcon, style: 'width: 20px; height: 20px;'})},
+    {
+        label: "生涯",
+        key: "career",
+        icon: () => h("img", { src: CareerIcon, style: "width: 20px; height: 20px;" }),
+    },
+    {
+        label: "战绩",
+        key: "record",
+        icon: () => h("img", { src: RecordIcon, style: "width: 20px; height: 20px;" }),
+    },
+    {
+        label: "助手",
+        key: "assistant",
+        icon: () => h("img", { src: AssistantIcon, style: "width: 20px; height: 20px;" }),
+    },
 ];
 
 const bottomMenuOptions = [
-  // {label: '玩家', key: 'personal', icon: () => h('img', {src: PersonalIcon, style: 'width: 20px; height: 20px;'})},
-  {label: '设置', key: 'setting', icon: () => h('img', {src: SettingIcon, style: 'width: 20px; height: 20px;'})},
+    // {label: '玩家', key: 'personal', icon: () => h('img', {src: PersonalIcon, style: 'width: 20px; height: 20px;'})},
+    {
+        label: "设置",
+        key: "setting",
+        icon: () => h("img", { src: SettingIcon, style: "width: 20px; height: 20px;" }),
+    },
 ];
 
 // 处理菜单变化
 const handleMenuChange = (key: string) => {
-  currentMenu.value = key;
+    currentMenu.value = key;
 };
+
+interface notification {
+    level: string;
+    message: string;
+}
+
+const notice = useNotification();
+
+onMounted(() => {
+    EventsOn("notification", async function (data: notification) {
+        switch (data.level) {
+            case "info":
+                notice.info({ content: data.message });
+                break;
+            case "warn":
+                notice.warning({ content: data.message });
+                break;
+            case "error":
+                notice.error({ content: data.message });
+                break;
+            case "success":
+                notice.error({ content: data.message });
+                break;
+        }
+    });
+});
 </script>
 
 <style scoped>
-
 .main-sider {
-  color: darkblue;
-  background-color: #a4b3ef;
-  transition: background-color 0.3s;
+    color: darkblue;
+    background-color: #a4b3ef;
+    transition: background-color 0.3s;
 }
 
 .bottom-menu {
-  margin-top: calc(100vh - 210px);
+    margin-top: calc(100vh - 210px);
 }
-
 </style>
